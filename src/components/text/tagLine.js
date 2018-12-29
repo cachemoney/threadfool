d651572ac2a431ad56a0c0f83f60dda0e25db512
Robin <cachebroker@gmail.com>
2018-12-29 15:10:24 -0600

    updated tagline

--- src/components/text/tagLine.js
+++ src/components/text/tagLine.js
@@ -5,7 +5,7 @@ const TagLine = () => {
     <div className='homePage'>
       <h1>
         <span className='minorLine'>Technical Notes On</span><br />
-        <span className='majorLine'>Software Design & Architecture</span>
+        <span className='majorLine'>Software Design & Data Architecture</span>
       </h1>
     </div>
   )
